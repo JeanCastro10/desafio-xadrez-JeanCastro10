d7aa2c11ec95046529f955b516859a9c114d7d9a
Jean Castro <jean.castro.42@gmail.com>
2026-02-21 04:37:26 +0000

    movimentaçoes completas

--- xadrez.c
+++ xadrez.c
@@ -2,40 +2,43 @@
 
 int main(){
     
-    int bispo = 5;
-    int torre = 5; 
-    int rainha = 8;
+    //declarando variaveis 
+    
+    int bispo = 5, torre = 5, rainha = 8;
     int b;
     int t = 1;
     int r = 1;
+
+    // ======= BISPO: 5 casas na diagonal superior direita =======
+    // Diagonal = combinação de duas direções básicas por passo (Cima + Direita)
     
-    printf("BISPO: 5 casas na diagonal superior direita\n");
+    printf("=====BISPO: 5 casas na diagonal superior direita=====\n");
     for ( b = 1; b <= bispo; b++)
     {
         printf("cima\n");
         printf("direita\n");
     }
     
-    printf("TORRE: 5 casas para a direita\n");
+    // ======= TORRE: 5 casas para a direita =======
+    
+    printf("=====TORRE: 5 casas para a direita=====\n");
 
     while (t <= torre)
     {   
             printf("Direita\n");
         t++;
-    }
+    }   
+    
+    // ======= RAINHA: 8 casas para a esquerda =======
+    printf("=====RAINHA: 8 casas para a esquerda=====\n");
+    
     do
     {
-        printf("esquerda\n");
+        printf("Esquerda\n");
         r++;
+
     } while (r <= rainha);
     
-    
-    
-
-    
-
-
-
-    return 0;
+      return 0;
 }
 
